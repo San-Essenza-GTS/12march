90c09508f295a71520310218a59a86f0df91d8f0
San-Essenza-GTS <sanjaiprasath222@gmail.com>
2025-03-12 11:59:17 +0530

    Add files via upload

---
+++ py.py
@@ -0,0 +1,9 @@
+a=int (input("integer 1"))
+b=int (input("integer 2"))
+c=int (input("integer 3"))
+d=a*b*c
+e=a+b+c
+f=d/e
+print(d)
+print(e)
+print(f)
---
+++ w.py
@@ -0,0 +1,12 @@
+a=3456
+
+if a<678:
+    print('first if')
+elif a>=678:
+    print('equal to and big')
+elif a==3450:
+    print('equal to the number')
+elif a<=3456:
+    print('perusu and equal')
+else:
+    print('ponga da dei')
